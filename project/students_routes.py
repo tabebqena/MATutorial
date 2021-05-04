d347ff39115cc298592e099b9d90644ec347525a
ahmad <tabebqena@gmail.com>
2021-05-04 14:25:38 +0300

    refactor large function

--- project/students_routes.py
+++ project/students_routes.py
@@ -13,10 +13,11 @@ def students():
 
 
 @app.route("/students/new", methods=["PUT"])
-def new_student():
+def add_student():
     students_data = get_data()["students"]
     info = request.form
 
+    # TODO FIX: more accurate validator
     if len(info) == 0:
         return jsonify({"state": "Error"}), http.HTTPStatus.NO_CONTENT
 
@@ -34,28 +35,35 @@ def new_student():
     return jsonify({"state": "Created", "id": id}), http.HTTPStatus.OK
 
 
-@app.route("/students/<string:id>", methods=["GET", "POST", "DELETE"])
-def student_info(id):
+@app.route("/students/<string:id>", methods=["GET"])
+def get_student_info(id):
     # show info of single student
+    students_data = get_data()["students"]
+    if students_data.get(id, None):
+        return jsonify(students_data[id]), http.HTTPStatus.OK
+    return jsonify({"state": "Error"}), http.HTTPStatus.NOT_FOUND
+
+
+@app.route("/students/<string:id>", methods=["POST"])
+def update_student(id):
     info = request.form
     students_data = get_data()["students"]
+    if students_data.get(id, None):
+        students_data[id] = info
+        update_students_data(students_data)
+        return jsonify({"state": "updated"}), http.HTTPStatus.OK
+    else:
+        # don't delete , it is an example
+        # abort(http.HTTPStatus.BAD_REQUEST, f"No student with this {id}")
+        return jsonify({"state": "Error"}), http.HTTPStatus.BAD_REQUEST
 
-    if request.method == "GET":
-        return jsonify(students_data[id]), http.HTTPStatus.OK
-        # sync , request, website_data, data.json
 
-    elif request.method == "POST":
-        if students_data.get(id, None):
-            students_data[id] = info
-            update_students_data(students_data)
-            return jsonify({"state": "updated"}), http.HTTPStatus.OK
-        else:
-            # abort(http.HTTPStatus.BAD_REQUEST, f"No student with this {id}")
-            return jsonify({"state": "Error"}), http.HTTPStatus.BAD_REQUEST
-    elif request.method == "DELETE":
-        if students_data.get(id, None):
-            del students_data[id]
-            update_students_data(students_data)
-            return jsonify({"state": "Deleted"}), http.HTTPStatus.OK
-        else:
-            return jsonify({"state": "Error"}), http.HTTPStatus.BAD_REQUEST
+@app.route("/students/<string: id>", methods=["DELETE"])
+def delete_student(id):
+    students_data = get_data()["students"]
+    if students_data.get(id, None):
+        del students_data[id]
+        update_students_data(students_data)
+        return jsonify({"state": "Deleted"}), http.HTTPStatus.OK
+    else:
+        return jsonify({"state": "Error"}), http.HTTPStatus.BAD_REQUEST
